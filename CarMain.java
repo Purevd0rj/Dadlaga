0574d27ab3c10a0ab8a698faae5287bd05030b16
Purevd0rj <133290098+Purevd0rj@users.noreply.github.com>
2025-06-04 13:16:42 +0800

    Update and rename Car.java to CarMain.java

--- CarMain.java
+++ CarMain.java
@@ -44,7 +44,7 @@ class Truck extends Vehicle {
     }
 }
 
-public class MainVehicle {
+public class CarMain {
     public static void main(String[] args) {
         Vehicle vehicle = new Vehicle();
         Car car = new Car();
